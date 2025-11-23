e109e258402b29ed7e68389e11f4dc27a49dc07a
tbrito06 <tbrito06@gmail.com>
2025-11-22 19:59:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Word list (uppercase)
 const words = [
-  "MOTHER","LOVE","LAWYER","BABA","GARDEN","XXXXX","LOYAL","FAMILY","FRIENDSHIP","PASSIONATE","ROSEMARY"
+  "MOTHER","LOVE","LAWYER","BABA","GARDEN","LOYAL","FAMILY","FRIENDSHIP","PASSIONATE","ROSEMARY"
 ];
 
 const size = 15;
